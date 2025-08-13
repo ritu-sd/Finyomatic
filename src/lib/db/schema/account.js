5197bb9e3a9a5ee7fb3f34ca7a2f6f7d11562dbe
ritu-sd <rituparno.g@rdsoftllp.com>
2025-08-13 17:13:25 +0530

    Update jsconfig.json to adjust module resolution paths for improved imports

---
+++ src/lib/db/schema/account.js
@@ -0,0 +1,28 @@
+const { integer, pgTable, text, primaryKey } = require("drizzle-orm/pg-core");
+const { users } = require("./users");
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => [
+    {
+      compoundKey: primaryKey({
+        columns: [account.provider, account.providerAccountId],
+      }),
+    },
+  ]
+);
