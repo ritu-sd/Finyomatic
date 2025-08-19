26e504a3935438fd4070165429c6eb79f2f3ca6d
ritu-sd <rituparno.g@rdsoftllp.com>
2025-08-20 01:00:33 +0530

    Add credentials provider to NextAuth for enhanced authentication options

--- auth.js
+++ auth.js
@@ -1,9 +1,24 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "./src/lib/db";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [Google],
+  providers: [
+    Google,
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+    }),
+  ],
+  authorize: async (credentials) => {},
 });
